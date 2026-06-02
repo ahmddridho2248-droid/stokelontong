5155149d84dafef527034bacfa2cae0b775cc0c1
ahmadridhobabai <ahmddridho2248@gmail.com>
2026-06-02 22:14:10 +0700

    Dashboard Completed

--- routes/web.php
+++ routes/web.php
@@ -8,17 +8,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', [\App\Http\Controllers\DashboardController::class, 'index'])
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    // Tambahin route kategori di sini
+    
     Route::resource('categories', \App\Http\Controllers\CategoryController::class);
-    Route::resource('products', ProductController::class); // Tambahin ini man!
     Route::resource('products', \App\Http\Controllers\ProductController::class);
 });
 
